c5055182f58b08cafe48c8b559c87c8438830d48
Jeremy Honl <jerhon@users.noreply.github.com>
2020-09-21 19:54:24 -0500

    Add animation to fade in the image titles.

---
+++ src/components/InViewModifier.tsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useRef } from "react"
+
+
+export interface InViewModifierProperties {
+  children?: JSX.Element | JSX.Element[];
+  className?: string;
+  inViewClassName: string;
+  sticky?: boolean;
+}
+
+export function InViewModifier({ children, className, inViewClassName, sticky }: InViewModifierProperties) {
+
+  let ref = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((e) => {
+        if (ref.current && e.target.isSameNode(ref.current)) {
+          if (e.isIntersecting) {
+            if (!ref.current.classList.contains(inViewClassName)) {
+               ref.current.classList.add(inViewClassName);
+               if (sticky) {
+                 observer.unobserve(e.target);
+               }
+            }
+          } else {
+            if (ref.current.classList.contains(inViewClassName)) {
+              if (!sticky) {
+                ref.current.classList.remove(inViewClassName);
+              }
+            }
+          }
+        }
+      });
+    });
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+  }, [ref.current])
+
+  return (<div className={className} ref={ref}>{children}</div>);
+
+}
