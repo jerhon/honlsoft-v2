60057b9cdef3f80dbdf1caeef3da25092a1c0f7b
Jeremy Honl <jerhon@users.noreply.github.com>
2021-01-18 12:47:32 -0600

    Fix URL again... ugh...

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: `Honlsoft`,
     description: `A personal and software development blog.`,
     author: `@gatsbyjs`,
-    image: '/img/honlsoft-thumbnail.jpg'
+    image: 'https://www.honlsoft.com/img/honlsoft-thumbnail.jpg'
   },
   plugins: [
     'gatsby-plugin-material-ui',
