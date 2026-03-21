55643dcae03a38edcb2b544f4659c1ee2c832adf
Jeremy Honl <jerhon@users.noreply.github.com>
2026-03-20 21:47:17 -0500

    Further updates.

--- .vitepress/theme/components/BlogArchive.vue
+++ .vitepress/theme/components/BlogArchive.vue
@@ -2,7 +2,7 @@
 import { computed } from "vue"
 
 import { data as posts } from "../data/posts.data"
-import { formatDate, tagToSlug } from "../lib/content"
+import PostList from "./PostList.vue"
 
 const props = withDefaults(defineProps<{ limit?: number }>(), {
   limit: undefined,
@@ -14,25 +14,5 @@ const visiblePosts = computed(() => {
 </script>
 
 <template>
-  <div class="archive-grid">
-    <article v-for="post in visiblePosts" :key="post.url" class="archive-card">
-      <h2 class="archive-card__title">
-        <a class="archive-card__link" :href="post.url">{{ post.title }}</a>
-      </h2>
-
-      <p class="archive-card__meta">{{ formatDate(post.date) }}</p>
-      <p class="archive-card__excerpt">{{ post.excerpt }}</p>
-
-      <div v-if="post.tags.length" class="archive-card__tags">
-        <a
-          v-for="tag in post.tags"
-          :key="tag"
-          class="tag-pill"
-          :href="`/tag/${tagToSlug(tag)}`"
-        >
-          {{ tag }}
-        </a>
-      </div>
-    </article>
-  </div>
+  <PostList :posts="visiblePosts" />
 </template>
