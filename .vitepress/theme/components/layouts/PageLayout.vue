b7399c6f64ef0a1d32f90fda33f79c4a0ffd1cf8
Jeremy Honl <jerhon@users.noreply.github.com>
2026-03-21 21:18:25 -0500

    Further layout tweaks

--- .vitepress/theme/components/layouts/PageLayout.vue
+++ .vitepress/theme/components/layouts/PageLayout.vue
@@ -1,3 +1,14 @@
 <template>
-    <Content />
+  <main class="hs-page-width px-4 sm:px-6 lg:px-8">
+    <div class="pageLayoutContent">
+      <Content />
+    </div>
+  </main>
 </template>
+
+<style scoped>
+.pageLayoutContent {
+  width: 100%;
+  min-width: 0;
+}
+</style>
